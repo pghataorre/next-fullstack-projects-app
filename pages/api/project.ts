64bcccf15ecf26d7b9af596f2368857bad2f2d64
Permy Ghataorre <pghataorre@and.digital>
2023-05-04 15:39:23 +0100

    Added in Date Value for Project Due Date

--- pages/api/project.ts
+++ pages/api/project.ts
@@ -5,11 +5,15 @@ export default async function handler(req, res) {
   const { name } = req.body;
   const user = await validateJWT(req.cookies[process.env.COOKIE_NAME]);
 
+
+
+
   try {
-    const projectData = await db.Project.create({
+    const projectData = await db.project.create({
       data: {
         name,
         ownerId: user.id,
+        due: addWeeksToDate(new Date(), 2),
       }
     })
 
@@ -23,3 +27,7 @@ export default async function handler(req, res) {
       res.json({ error })}
 }
 
+const addWeeksToDate = (dateObj: Date,numberOfWeeks: number) => {
+  dateObj.setDate(dateObj.getDate() + numberOfWeeks * 7);
+  return dateObj;
+}
